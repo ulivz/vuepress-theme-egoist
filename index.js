9d6c6727459b0a22651eb76ba17d27818f03c909
ULIVZ <472590061@qq.com>
2018-10-08 00:28:46 +0800

    fix: specify global compoennts directory since alpha.6 has removed it.

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+const path = require('path')
+
 module.exports = {
   plugins: [
     '@vuepress/blog',
     '@vuepress/pagination',
-    ['@vuepress/search', {
+    [
+      '@vuepress/search', {
       searchMaxSuggestions: 10
+    }],
+    [
+      '@vuepress/register-components', {
+      componentsDir: [
+        path.resolve(this.sourceDir, '.vuepress/components')
+      ]
     }]
   ]
 }
