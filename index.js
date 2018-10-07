02503736bd85bc5e3da77f497e372d425801e726
ULIVZ <472590061@qq.com>
2018-10-08 00:20:08 +0800

    feat: update to alpha.6 theme API

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 module.exports = {
-  layoutDir: 'layouts',
   plugins: [
     '@vuepress/blog',
     '@vuepress/pagination',
     ['@vuepress/search', {
       searchMaxSuggestions: 10
     }]
-  ],
-  palette: {
-    $accentColor: '#b58900',
-    $textColor: '#586e75',
-    $borderColor: '#eaecef',
-    $codeBgColor: '#282c34',
-    $arrowBgColor: '#ccc'
-  }
+  ]
 }
